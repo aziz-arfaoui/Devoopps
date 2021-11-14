2857ba8ae8eadf7b25f59904567581aa42fdc88c
aziz-arfaoui <aziz99arfaoui@gmail.com>
2021-11-14 17:49:38 +0100

    initialisation du projet timesheet-devops

---
+++ src/test/java/tn/esprit/spring/services/UserServiceImplTest.java
@@ -0,0 +1,25 @@
+package tn.esprit.spring.services;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import tn.esprit.spring.entities.User;
+
+@SpringBootTest
+public class UserServiceImplTest {
+	
+	@Autowired
+	IUserService us;
+	
+	@Test
+	public void TestRetrieveAllUsers() {
+		List<User> listUsers = us.retrieveAllUsers();
+		//Assertions.assertEquals(2, listUsers.size());
+	}
+	
+
+}
